c7e97c1a01c27646ed46f0e59173c54b3d352594
Rizqy Hidayat <rizqy22@gmail.com>
2023-05-11 08:21:26 +0700

    implement rate limiter

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,23 @@
 import { ImageAnnotatorClient } from "@google-cloud/vision";
 import { onRequest } from "firebase-functions/v2/https";
+import { initializeApp } from "firebase-admin/app";
+import { getDatabase } from "firebase-admin/database";
+import { FirebaseFunctionsRateLimiter } from "firebase-functions-rate-limiter";
+
+initializeApp();
+
+const limiter = FirebaseFunctionsRateLimiter.withRealtimeDbBackend(
+  {
+    name: "rate_limiter_collection",
+    maxCalls: 200,
+    periodSeconds: 5,
+  },
+  getDatabase()
+);
 
 export const ocr = onRequest(async (request, response) => {
+  await limiter.rejectOnQuotaExceededOrRecordUsage();
+
   try {
     const visionClient = new ImageAnnotatorClient();
     const visionResponse = await visionClient.annotateImage({
